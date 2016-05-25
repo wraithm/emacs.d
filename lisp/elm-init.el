1e41a4b60e6118da84f5afd67a99b910a4918f42
Matthew Wraith <wraithm@gmail.com>
2016-05-25 12:50:39 -0500

    A lot of stuff

---
+++ lisp/elm-init.el
@@ -0,0 +1,27 @@
+;; elm-init
+;; Matthew Wraith <wraithm@gmail.com>
+
+;; Install packages
+(setq my-elm-packages
+      '(elm-mode
+        flycheck-elm
+        ;; company-ghc
+        ))
+(mapc #'package-install my-elm-packages)
+
+(require 'elm-mode)
+(require 'flycheck)
+(require 'flycheck-elm)
+
+(with-eval-after-load 'flycheck
+      '(add-hook 'flycheck-mode-hook #'flycheck-elm-setup))
+
+(with-eval-after-load 'company
+  (add-to-list 'company-backends 'company-elm))
+
+(add-hook 'elm-mode-hook
+          (lambda ()
+            (setq company-backends '(company-elm))))
+
+(message "Loading elm-init... Done.")
+(provide 'elm-init)
