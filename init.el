f8135a583a660e43c26912beaf5679669afbc9a0
Matthew Wraith <wraithm@gmail.com>
2016-08-08 14:36:55 -0500

    removing theme file adding compile commands

--- init.el
+++ init.el
@@ -35,6 +35,7 @@
         dash-at-point
         multi-term
         terraform-mode
+        vagrant-tramp
         nlinum-relative
         json-mode
         rainbow-delimiters
@@ -49,7 +50,32 @@
 
 ;; My custom code
 (add-to-list 'load-path "~/.emacs.d/lisp")
-(load "~/.emacs.d/theme.el")
+
+
+;; Font
+(set-frame-font "Inconsolata-14")
+
+;; (setq mac-allow-anti-aliasing nil)
+;; (set-frame-font "Terminus (TTF)-14")
+;; (set-face-attribute 'default nil :family "Terminus (TTF)" :height 160)
+
+
+;; Theme
+(setq
+ solarized-use-variable-pitch nil
+ solarized-scale-org-headlines nil)
+(load-theme 'solarized-light)
+
+;; (if window-system
+;;     (load-theme 'base16-tomorrow-dark))
+
+;; (require 'moe-theme)
+;; (setq moe-theme-highlight-buffer-id t) ; moe-theme settings
+;; (setq moe-theme-resize-markdown-title '(1.5 1.4 1.3 1.2 1.0 1.0))
+;; (setq moe-theme-resize-org-title '(1.5 1.4 1.3 1.2 1.1 1.0 1.0 1.0 1.0))
+;; (setq moe-theme-resize-rst-title '(1.5 1.4 1.3 1.2 1.1 1.0))
+;; (moe-dark)
+
 
 ;; PATH
 (require 'exec-path-from-shell)
@@ -80,6 +106,9 @@
 (define-key evil-motion-state-map (kbd "C-k") 'evil-window-up)
 (define-key evil-motion-state-map (kbd "C-l") 'evil-window-right)
 (global-set-key (kbd "M-;") 'evilnc-comment-or-uncomment-lines)
+(require 'evil-nerd-commenter)
+(require 'evil-nerd-commenter-operator)
+(define-key evil-normal-state-map (kbd "gc") 'evilnc-comment-operator)
 ;; (global-set-key (kbd "C-c l") 'evilnc-quick-comment-or-uncomment-to-the-line)
 ;; (global-set-key (kbd "C-c c") 'evilnc-copy-and-comment-lines)
 ;; (global-set-key (kbd "C-c p") 'evilnc-comment-or-uncomment-paragraphs)
@@ -87,17 +116,18 @@
 (evil-ex-define-cmd "Q" 'save-buffers-kill-terminal)
 (global-evil-surround-mode 1)
 
-(evil-define-operator evil-delete-without-register (beg end type yank-handler)
-  "Delete from beg to end and send to \"_ register"
-  (interactive "<R><y>")
-  (evil-delete beg end type ?_ yank-handler))
+;; Stamp operator
+;; (evil-define-operator evil-delete-without-register (beg end type yank-handler)
+;;   "Delete from beg to end and send to \"_ register"
+;;   (interactive "<R><y>")
+;;   (evil-delete beg end type ?_ yank-handler))
 
-(evil-define-operator evil-stamp (beg end)
-  "Replace text-object with 0th register contents"
-  (evil-delete-without-register beg end)
-  (evil-paste-from-register ?0))
+;; (evil-define-operator evil-stamp (beg end)
+;;   "Replace text-object with 0th register contents"
+;;   (evil-delete-without-register beg end)
+;;   (evil-paste-from-register ?0))
 
-(define-key evil-normal-state-map (kbd "S") 'evil-stamp)
+;; (define-key evil-normal-state-map (kbd "S") 'evil-stamp)
 
 ;; line numbers
 ;; (global-linum-mode t)
@@ -112,6 +142,8 @@
 ;; key-bindings
 (global-set-key (kbd "C-c g") 'ag)
 (global-set-key (kbd "C-c a") 'align-regexp)
+(global-set-key (kbd "C-c C-c") 'compile)
+(global-set-key (kbd "C-c r") 'recompile)
 
 ;; Variables
 (setq make-backup-files nil)
@@ -268,7 +300,7 @@
     ("99953b61ecd4c3e414a177934e888ce9ee12782bbaf2125ec2385d5fd732cbc2" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "113ae6902d98261317b5507e55ac6e7758af81fc4660c34130490252640224a2" "d76af04d97252fafacedc7860f862f60d61fdcfbd026aeba90f8d07d8da51375" "01d8c9140c20e459dcc18addb6faebd7803f7d6c46d626c7966d3f18284c4502" "3328e7238e0f6d0a5e1793539dfe55c2685f24b6cdff099c9a0c185b71fbfff9" "75c0b1d2528f1bce72f53344939da57e290aa34bea79f3a1ee19d6808cb55149" "51e228ffd6c4fff9b5168b31d5927c27734e82ec61f414970fc6bcce23bc140d" "3f78849e36a0a457ad71c1bda01001e3e197fe1837cb6eaa829eb37f0a4bdad5" "26614652a4b3515b4bbbb9828d71e206cc249b67c9142c06239ed3418eff95e2" "133222702a3c75d16ea9c50743f66b987a7209fb8b964f2c0938a816a83379a0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(package-selected-packages
    (quote
-    (rainbow-delimiters json-mode evil-nerd-commenter sr-speedbar latex-preview-pane ansible-doc company-ansible jinja2-mode haskell-mode yasnippet company flycheck evil yaml-mode w3m ujelly-theme twilight-theme terraform-mode solarized-theme smex projectile paredit nlinum-relative multi-term moe-theme markdown-mode+ magit intero hindent haskell-snippets flycheck-elm flx-ido exec-path-from-shell evil-surround evil-org erlang elm-mode dash-at-point base16-theme auctex ag))))
+    (vagrant-tramp rainbow-delimiters json-mode evil-nerd-commenter sr-speedbar latex-preview-pane ansible-doc company-ansible jinja2-mode haskell-mode yasnippet company flycheck evil yaml-mode w3m ujelly-theme twilight-theme terraform-mode solarized-theme smex projectile paredit nlinum-relative multi-term moe-theme markdown-mode+ magit intero hindent haskell-snippets flycheck-elm flx-ido exec-path-from-shell evil-surround evil-org erlang elm-mode dash-at-point base16-theme auctex ag))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
