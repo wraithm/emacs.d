6632d527031bafaa08491997498752bc71897a9c
Matthew Wraith <wraithm@gmail.com>
2016-08-07 19:02:44 -0500

    moving theme code into separate file, moving sections around

--- init.el
+++ init.el
@@ -49,6 +49,7 @@
 
 ;; My custom code
 (add-to-list 'load-path "~/.emacs.d/lisp")
+(load "~/.emacs.d/theme.el")
 
 ;; PATH
 (require 'exec-path-from-shell)
@@ -98,6 +99,15 @@
 
 (define-key evil-normal-state-map (kbd "S") 'evil-stamp)
 
+;; line numbers
+;; (global-linum-mode t)
+(require 'nlinum-relative)
+(nlinum-relative-setup-evil)
+(global-nlinum-relative-mode)
+;; (add-hook 'prog-mode-hook 'nlinum-relative-mode)
+(setq nlinum-relative-redisplay-delay 0)
+(setq nlinum-relative-current-symbol "")
+(setq nlinum-relative-offset 0) 
 
 ;; key-bindings
 (global-set-key (kbd "C-c g") 'ag)
@@ -121,6 +131,9 @@
 (global-font-lock-mode t)
 ;; (global-hl-line-mode t)
 
+;; Fundamental
+(add-hook 'fundamental-mode-hook 'flyspell-mode)
+(add-hook 'fundamental-mode-hook 'turn-on-auto-fill)
 
 ;; shell
 (require 'multi-term)
@@ -138,7 +151,6 @@
    (setq yas-dont-activate t)))
 ;; (multi-term)
 
-
 ;; smex / ido
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
@@ -150,7 +162,6 @@
  ido-enable-flex-matching t
  ;; ido-use-faces nil
  ido-max-window-height 1)
-
 
 ;; ibuffer
 (global-set-key (kbd "C-x C-b") 'ibuffer)
@@ -179,60 +190,9 @@
 (setq-default ibuffer-show-empty-filter-groups nil)
 (add-hook 'ibuffer-hook (lambda () (ibuffer-major-mode-group-hook)))
 
-
-;; line numbers
-;; (global-linum-mode t)
-(require 'nlinum-relative)
-(nlinum-relative-setup-evil)
-(global-nlinum-relative-mode)
-;; (add-hook 'prog-mode-hook 'nlinum-relative-mode)
-(setq nlinum-relative-redisplay-delay 0)
-(setq nlinum-relative-current-symbol "")
-(setq nlinum-relative-offset 0) 
-
-
 ;; Paren
 (show-paren-mode t)
 (setq show-paren-style 'expression)
-
-;; Font
-;; (setq mac-allow-anti-aliasing nil)
-;; (set-frame-font "Terminus (TTF)-14")
-;; (set-face-attribute 'default nil :family "Terminus (TTF)" :height 160)
-(set-frame-font "Inconsolata-14")
-
-;; Theme
-(setq
- solarized-use-variable-pitch nil
- solarized-scale-org-headlines nil)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("99953b61ecd4c3e414a177934e888ce9ee12782bbaf2125ec2385d5fd732cbc2" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "113ae6902d98261317b5507e55ac6e7758af81fc4660c34130490252640224a2" "d76af04d97252fafacedc7860f862f60d61fdcfbd026aeba90f8d07d8da51375" "01d8c9140c20e459dcc18addb6faebd7803f7d6c46d626c7966d3f18284c4502" "3328e7238e0f6d0a5e1793539dfe55c2685f24b6cdff099c9a0c185b71fbfff9" "75c0b1d2528f1bce72f53344939da57e290aa34bea79f3a1ee19d6808cb55149" "51e228ffd6c4fff9b5168b31d5927c27734e82ec61f414970fc6bcce23bc140d" "3f78849e36a0a457ad71c1bda01001e3e197fe1837cb6eaa829eb37f0a4bdad5" "26614652a4b3515b4bbbb9828d71e206cc249b67c9142c06239ed3418eff95e2" "133222702a3c75d16ea9c50743f66b987a7209fb8b964f2c0938a816a83379a0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
- '(package-selected-packages
-   (quote
-    (rainbow-delimiters json-mode evil-nerd-commenter sr-speedbar latex-preview-pane ansible-doc company-ansible jinja2-mode haskell-mode yasnippet company flycheck evil yaml-mode w3m ujelly-theme twilight-theme terraform-mode solarized-theme smex projectile paredit nlinum-relative multi-term moe-theme markdown-mode+ magit intero hindent haskell-snippets flycheck-elm flx-ido exec-path-from-shell evil-surround evil-org erlang elm-mode dash-at-point base16-theme auctex ag))))
-
-;; (if window-system
-;;     (load-theme 'base16-tomorrow-dark))
-
-;; (require 'moe-theme)
-;; (setq moe-theme-highlight-buffer-id t) ; moe-theme settings
-;; (setq moe-theme-resize-markdown-title '(1.5 1.4 1.3 1.2 1.0 1.0))
-;; (setq moe-theme-resize-org-title '(1.5 1.4 1.3 1.2 1.1 1.0 1.0 1.0 1.0))
-;; (setq moe-theme-resize-rst-title '(1.5 1.4 1.3 1.2 1.1 1.0))
-;; (moe-dark)
-
-(load-theme 'solarized-light)
-
-;; Fundamental
-(add-hook 'fundamental-mode-hook 'flyspell-mode)
-(add-hook 'fundamental-mode-hook 'turn-on-auto-fill)
 
 ;; Company
 (require 'company)
@@ -295,6 +255,20 @@
 
 ;; org-mode
 (require 'org-init)
+
+
+;; Don't touch this stuff below
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("99953b61ecd4c3e414a177934e888ce9ee12782bbaf2125ec2385d5fd732cbc2" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "113ae6902d98261317b5507e55ac6e7758af81fc4660c34130490252640224a2" "d76af04d97252fafacedc7860f862f60d61fdcfbd026aeba90f8d07d8da51375" "01d8c9140c20e459dcc18addb6faebd7803f7d6c46d626c7966d3f18284c4502" "3328e7238e0f6d0a5e1793539dfe55c2685f24b6cdff099c9a0c185b71fbfff9" "75c0b1d2528f1bce72f53344939da57e290aa34bea79f3a1ee19d6808cb55149" "51e228ffd6c4fff9b5168b31d5927c27734e82ec61f414970fc6bcce23bc140d" "3f78849e36a0a457ad71c1bda01001e3e197fe1837cb6eaa829eb37f0a4bdad5" "26614652a4b3515b4bbbb9828d71e206cc249b67c9142c06239ed3418eff95e2" "133222702a3c75d16ea9c50743f66b987a7209fb8b964f2c0938a816a83379a0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(package-selected-packages
+   (quote
+    (rainbow-delimiters json-mode evil-nerd-commenter sr-speedbar latex-preview-pane ansible-doc company-ansible jinja2-mode haskell-mode yasnippet company flycheck evil yaml-mode w3m ujelly-theme twilight-theme terraform-mode solarized-theme smex projectile paredit nlinum-relative multi-term moe-theme markdown-mode+ magit intero hindent haskell-snippets flycheck-elm flx-ido exec-path-from-shell evil-surround evil-org erlang elm-mode dash-at-point base16-theme auctex ag))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
---
+++ theme.el
@@ -0,0 +1,21 @@
+;; Font
+;; (setq mac-allow-anti-aliasing nil)
+;; (set-frame-font "Terminus (TTF)-14")
+;; (set-face-attribute 'default nil :family "Terminus (TTF)" :height 160)
+(set-frame-font "Inconsolata-14")
+
+;; Theme
+(setq
+ solarized-use-variable-pitch nil
+ solarized-scale-org-headlines nil)
+(load-theme 'solarized-light)
+
+;; (if window-system
+;;     (load-theme 'base16-tomorrow-dark))
+
+;; (require 'moe-theme)
+;; (setq moe-theme-highlight-buffer-id t) ; moe-theme settings
+;; (setq moe-theme-resize-markdown-title '(1.5 1.4 1.3 1.2 1.0 1.0))
+;; (setq moe-theme-resize-org-title '(1.5 1.4 1.3 1.2 1.1 1.0 1.0 1.0 1.0))
+;; (setq moe-theme-resize-rst-title '(1.5 1.4 1.3 1.2 1.1 1.0))
+;; (moe-dark)
