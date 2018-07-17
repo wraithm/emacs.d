70926611f99ae80bd048736676dc7b45a56dc9a2
Matthew Wraith <wraithm@gmail.com>
2018-07-17 17:13:58 -0500

    Global flycheck stuff

--- init.el
+++ init.el
@@ -79,6 +79,9 @@
   (advice-add 'ns-new-frame :after '(scroll-bar-mode -1))
   (advice-add 'ns-new-frame :after #'toggle-frame-fullscreen))
 
+(require 'flycheck)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+(setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
 
 ;; evil-mode
 (require 'evil)
