559a70a5c741be0b829fcd2d6d9334d16dedd72f
Matthew Wraith <wraithm@gmail.com>
2018-08-15 14:06:51 -0500

    Cleaning up elisp

--- lisp/rust-init.el
+++ lisp/rust-init.el
@@ -1,13 +1,14 @@
-(setq my-rust-packages
+(defvar my-rust-packages
       '(rust-mode
         racer
+        cargo
         flycheck
         flycheck-rust))
 (mapc #'package-install my-rust-packages)
 
-(require 'company)
 (require 'racer)
 (require 'rust-mode)
+(require 'cargo)
 (require 'eldoc)
 (require 'flycheck)
 (require 'flycheck-rust)
@@ -16,14 +17,14 @@
  'rust-mode-hook
  (lambda ()
    (subword-mode)
-   (company-mode)
    (racer-mode)
    (cargo-minor-mode)
    (flycheck-mode)
-   (local-set-key (kbd "C-c <tab>") #'rust-format-buffer))
- (setq
-  racer-cmd "~/.cargo/bin/racer"
-  racer-rust-src-path "/Users/mwraith/src/Rust/rust/src"))
+   (local-set-key (kbd "C-c <tab>") #'rust-format-buffer)))
+
+(setq
+ racer-cmd "~/.cargo/bin/racer"
+ racer-rust-src-path "/Users/mwraith/src/Rust/rust/src")
 
 (add-hook 'racer-mode-hook #'eldoc-mode)
 (add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
