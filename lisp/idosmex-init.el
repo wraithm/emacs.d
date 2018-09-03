2f4e64df514eb018d8cd97c293da1047d86e8fb4
Matthew Wraith <wraithm@gmail.com>
2018-09-03 16:30:45 -0500

    Separated ido and smex config to separate lisp file

---
+++ lisp/idosmex-init.el
@@ -0,0 +1,29 @@
+(defvar my-idosmex-packages
+  '(smex
+    flx-ido))
+(mapc #'package-install my-idosmex-packages)
+
+(require 'flx-ido)
+
+(ido-mode t)
+(ido-everywhere t)
+(flx-ido-mode t)
+
+(setq-default
+ ;; ido-use-faces nil
+ ido-enable-flex-matching t
+ ;; ido-max-window-height 1
+ ido-create-new-buffer 'always)
+
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+
+(require 'evil-leader)
+(evil-leader/set-key
+  "K" 'ido-kill-buffer
+  "x" 'smex)
+
+(setq-default yas-prompt-functions '(yas-ido-prompt yas-dropdown-prompt)) ; use ido for multiple snippets
+
+(message "Loading smexido-init... Done.")
+(provide 'smexido-init)
